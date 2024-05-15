e71ff9123f73352e6397a00254f5b04d63286dd6
Par-zeus <143513965+Par-zeus@users.noreply.github.com>
2024-05-15 10:11:53 +0530

    Update NQueens.c

--- DAA Exam Codes/Backtracking/NQueens.c
+++ DAA Exam Codes/Backtracking/NQueens.c
@@ -1,89 +1,73 @@
 #include <stdio.h>
-#include <stdbool.h>
-#define n 4
+#include <stdlib.h>
 
-void print_board(int board[][n])
+int place(int r, int c, int column[])
 {
-    for (int i = 0; i < n; i++)
+    for (int j = 1; j < r; j++)
     {
-        for (int j = 0; j < n; j++)
+        if (column[j] == c || abs(column[j] - c) == abs(j - r))
+            return 0;
+    }
+    return 1;
+}
+
+int printboard(int n, int column[])
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
         {
-            if (board[i][j] == 1)
+            if (column[i] == j)
             {
                 printf(" Q ");
             }
             else
             {
-                printf(" ✔ ");
+                printf(" . ");
             }
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-bool isSafe(int board[][n], int row, int col)
+void nqueens(int r, int n, int column[], int *flag)
 {
-    int i, j;
-    // check for same row
-    for (i = 0; i < col; i++)
+    // printf("\nSolution %d",*(++flag));
+    for (int c = 1; c <= n; c++)
     {
-        if (board[row][i])
-            return false;
-    }
-
-    // check for upper triangle
-    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-    {
-        // printf("%d %d \n",row,col);
-        if (board[i][j])
-            return false;
-    }
-
-    // check for lower triangle
-    for (i = row, j = col; i < n && j >= 0; i++, j--)
-    {
-        if (board[i][j])
-            return false;
-    }
-    return true;
-}
-
-bool Solve_until(int board[][n], int col)
-{
-    if (col >= n)
-        return true;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (isSafe(board, i, col))
+        if (place(r, c, column))
         {
-            board[i][col] = 1;
+            column[r] = c;
 
-            if (Solve_until(board, col + 1))
-                return true;
-            // If placing the queen at board[i][col] doesn't lead to a solution,
-            // then remove the queen from board[i][col]
-            board[i][col] = 0;
+            if (r == n)
+            {
+                printboard(n, column);
+                *flag = 1;
+            }
+            else
+            {
+                nqueens(r + 1, n, column, flag);
+            }
         }
     }
-    return false;
-}
-
-bool NQueens()
-{
-    int board[n][n] = {0};
-    if (!Solve_until(board, 0))
-    {
-        printf("\nSolution Doesn't exist");
-        return false;
-    }
-
-    print_board(board);
-    return true;
 }
 
 int main()
 {
-    NQueens();
+    int n;
+    printf("\nEnter the number of queens: ");
+    scanf("%d", &n);
+    int column[n];
+    int flag = 0; // Initialize flag
+    for (int i = 0; i < n; i++)
+    {
+        column[i] = 0;
+    }
+    nqueens(1, n, column, &flag);
+    if (!flag) // Check if no solution is found
+    {
+        printf("\nNo solution found.\n");
+    }
     return 0;
 }
